1e60653a4372302424737b1d1fda62d35758d36e
Kari <kouglas2@gmail.com>
2022-03-04 15:39:53 -0500

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,4 +1,12 @@
-# TODO
+# this program says hello and asks for my name.
 
-name = input("What's your name? ")
-print(f"hello, {name}")
+print('Hello World')
+print('What is your name?') # ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of your name is:')
+print(len(myName))
+print('What is your age?') # ask of their age
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.')
+ 
